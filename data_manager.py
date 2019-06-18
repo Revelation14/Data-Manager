4dc58f68362cb1b916934931411a2e47ad63f799
Ketan Khatwal <ketan.khatwal@gmail.com>
2019-06-19 00:34:11 +0530

    modified_movement

--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,14 @@
 import os,sys
 
+def existing_folder(ff,main_folder):
+    for file in os.listdir("Data\\{}".format(ff)):
+                if file in os.listdir("Data\\{}\\{}".format(main_folder,ff)):
+                    fn,fext=os.path.splitext(file)
+                    os.rename("Data\\{}\\{}".format(ff,file),"Data\\{}\\{}\\{}".format(main_folder,ff,fn+"1"+fext))
+                else:
+                    os.rename("Data\\{}\\{}".format(ff,file),"Data\\{}\\{}\\{}".format(main_folder,ff,file))
+    os.rmdir("Data\\{}".format(ff))
+
 for f in os.listdir():
     fn,fext=os.path.splitext(f)
     if not os.path.isdir("Data"):
@@ -30,19 +39,40 @@ for f in os.listdir():
         print(f)
 for ff in os.listdir("Data"):
     if ff in ['mp3','wav']:
-        os.rename("Data\\{}".format(ff),"Data\\Music\\{}".format(ff))
+        if ff in os.listdir("Data\\Music"):
+            existing_folder(ff,"Music")
+        else:
+            os.rename("Data\\{}".format(ff),"Data\\Music\\{}".format(ff))
     elif ff in ['mp4','avi','mpeg','flv','wmv','3gp']:
-        os.rename("Data\\{}".format(ff),"Data\\Videos\\{}".format(ff))
+        if ff in os.listdir("Data\\Videos"):
+            existing_folder(ff,"Videos")
+        else:
+            os.rename("Data\\{}".format(ff),"Data\\Videos\\{}".format(ff))
     elif ff in ['csv','docx','pdf','rtf','py']:
-        os.rename("Data\\{}".format(ff),"Data\\Documents\\{}".format(ff))
+        if ff in os.listdir("Data\\Documents"):
+            existing_folder(ff,"Documents")
+        else:
+            os.rename("Data\\{}".format(ff),"Data\\Documents\\{}".format(ff))
     elif ff in ['exe']:
-        os.rename("Data\\{}".format(ff),"Data\\Programs\\{}".format(ff))
+        if ff in os.listdir("Data\\Programs"):
+            existing_folder(ff,"Programs")
+        else:
+            os.rename("Data\\{}".format(ff),"Data\\Programs\\{}".format(ff))
     elif ff in ['rar','zip','iso']:
-        os.rename("Data\\{}".format(ff),"Data\\Compressed\\{}".format(ff))    
+        if ff in os.listdir("Data\\Compressed"):
+            existing_folder(ff,"Compressed")
+        else:
+            os.rename("Data\\{}".format(ff),"Data\\Compressed\\{}".format(ff))    
     elif ff in ['jpg','png','jpeg','ico']:
-        os.rename("Data\\{}".format(ff),"Data\\Photos\\{}".format(ff))
+        if ff in os.listdir("Data\\Photos"):
+            existing_folder(ff,"Photos")
+        else:
+            os.rename("Data\\{}".format(ff),"Data\\Photos\\{}".format(ff))
     elif ff not in ['Music','Videos','Documents','Programs','Others','Photos','Compressed']:
-        os.rename("Data\\{}".format(ff),"Data\\Others\\{}".format(ff))
-
+        if ff in os.listdir("Data\\Others"):
+            existing_folder(ff,"Others")
+        else:
+            os.rename("Data\\{}".format(ff),"Data\\Others\\{}".format(ff))
+    
 
             
